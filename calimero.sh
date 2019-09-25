68408bc9d925b414a82d1a1d638fc629c45424d5
Race666 <michael@albert-hetzles.de>
2019-09-25 12:24:32 +0200

    Update calimero.sh
    type :-(

--- calimero.sh
+++ calimero.sh
@@ -220,7 +220,7 @@ apt-get -y install build-essential cmake
 apt-get -y install automake autoconf libtool 
 apt-get -y install dirmngr 
 apt-get -y install net-tools software-properties-common xmlstarlet debconf-utils crudini
-apt-get -y unzip
+apt-get -y install unzip
 ########################### Java ##############################################
 if [ $HARDWARE = "Raspi" ]; then
     apt-get install oracle-java8-jdk
